0a69f24cdfc3cbbc9f50f87ba2a06e4d34b215a6
VIJAYASHUNMUGAMS <47299186+VIJAYASHUNMUGAMS@users.noreply.github.com>
2023-04-07 15:51:55 +0530

    Delete MyJavaProject directory

--- MyJavaProject/src/com/pkg/controller/PatientInterface.java
+++
@@ -1,9 +0,0 @@
-package com.pkg.controller;
-
-public interface PatientInterface {
-	
-	//To Jus hold the method declaration/definition.. Child class needs to implement the methods.
-	public void AddPatient();
-
-	public void ViewPatient();
-}
